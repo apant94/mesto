3e91a1267dd15986400c1a34ca6a8fff9a8c4030
Anna Panteleeva <apanteleeva94@gmail.com>
2022-07-11 15:56:40 +0300

    fix after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const elementTemplate = document.querySelector('#element').content;
 const elements = document.querySelector('.elements__list');
 const element = document.querySelector('.element');
@@ -34,14 +7,14 @@ const popupEdit = document.querySelector('.popup_edit');
 const popupAdd = document.querySelector('.popup_add');
 const popupImage = document.querySelector('.popup_image');
 
-const editBtn = document.querySelector('.profile__edit');
-const addBtn = document.querySelector('.profile__add');
-const closeBtn = document.querySelectorAll('.popup__close');
+const btnEdit = document.querySelector('.profile__edit');
+const btnAdd = document.querySelector('.profile__add');
+const btnClose = document.querySelectorAll('.popup__close');
 
 const nameInput = popupEdit.querySelector('.popup__text_value_name');
 const jobInput = popupEdit.querySelector('.popup__text_value_job');
-const getName = document.querySelector('.profile__name');
-const getJob = document.querySelector('.profile__job');
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
 
 const placeInput = popupAdd.querySelector('.popup__text_value_place');
 const linkInput = popupAdd.querySelector('.popup__text_value_link');
@@ -54,16 +27,19 @@ renderItems();
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-  nameInput.value = getName.textContent;
-  jobInput.value = getJob.textContent;
 };
+
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 };
 
-editBtn.addEventListener('click', () => {openPopup(popupEdit)});
-addBtn.addEventListener('click', () => {openPopup(popupAdd)});
-closeBtn.forEach((item) => {
+btnEdit.addEventListener('click', () => {
+  openPopup(popupEdit);
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+});
+btnAdd.addEventListener('click', () => {openPopup(popupAdd)});
+btnClose.forEach((item) => {
   const popup = item.closest('.popup');
   item.addEventListener('click', () => {closePopup(popup)});
 });
@@ -72,7 +48,7 @@ function createItem(newItem) {
   const card = elementTemplate.querySelector('.element').cloneNode(true);
   card.querySelector('.element__title').textContent = newItem.name;
   card.querySelector('.element__photo').src = newItem.link;
-  elements.append(card);
+  card.querySelector('.element__photo').alt = newItem.name;
 
   card.querySelector('.element__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('element__like_active');
@@ -97,8 +73,8 @@ function renderItem(newItem) {
 
 function editFormSubmitHandler(evt) {
   evt.preventDefault();
-  getName.textContent = nameInput.value;
-  getJob.textContent = jobInput.value;
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
   closePopup(popupEdit);
 };
 
