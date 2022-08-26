c70f407230f554181369323fcc6ba08b98842e47
Anna Panteleeva <apanteleeva94@gmail.com>
2022-08-26 17:00:22 +0300

    dev: CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import { initialCards, popupEdit, popupAdd, btnEdit, btnAdd, nameInput, jobInput, nameProfile, jobProfile, placeInput, linkInput, popupImage } from '../utils/constants.js';
 import Card from '../components/Card.js';
 import Section from '../components/Section.js';
