3dbe27ed8b48cbcc030157dfbe9774c5115aa776
Anna Panteleeva <apanteleeva94@gmail.com>
2022-08-26 17:11:16 +0300

    dev: project 8

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,9 @@ module.exports = {
     {
       test: /\.css$/,
       use: [MiniCssExtractPlugin.loader, {
-        loader: 'css-loader'
-      }]
+        loader: 'css-loader',
+        options: { importLoaders: 1 }
+      },'postcss-loader']
     }
     ]
   },
